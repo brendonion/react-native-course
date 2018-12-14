cd50b5edd5bcb8b94d4d097ef71728bc50e1e9e4
Brendan Walker <brendan@freshworks.io>
2018-12-13 16:27:22 -0800

    Finishes up tp Section 2, lecture 37.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,66 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+
+import PlaceList from './components/PlaceList/PlaceList';
+import PlaceInput from './components/PlaceInput/PlaceInput';
+import PlaceDetail from './components/PlaceDetail/PlaceDetail';
 
 export default class App extends React.Component<{}> {
+
+  state = {
+    places: [],
+    selectedPlace: null,
+  }
+
+  placeAddedHandler = (placeName: string) => {
+    this.setState((prevState) => ({
+      places: [
+        { 
+          key: Math.random(), 
+          name: placeName, 
+          image: {
+            uri: 'https://lumiere-a.akamaihd.net/v1/images/image_da9047cc.jpeg?region=0%2C45%2C1280%2C720&width=960',
+          } 
+        }, 
+        ...prevState.places
+      ],
+    }));
+  }
+
+  placeSelectedHandler = (key: number) => {
+    this.setState((prevState) => ({
+      selectedPlace: prevState.places.find((place: object) => place.key === key),
+    }));
+  }
+
+  placeDeletedHandler = () => {
+    this.setState((prevState) => ({
+      places: prevState.places.filter((place: object) => place.key !== prevState.selectedPlace.key),
+      selectedPlace: null,
+    }));
+  }
+
+  modalClosedHandler = () => {
+    this.setState({ selectedPlace: null });
+  }
+
   render() {
+    const { places, selectedPlace } = this.state;
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.ts to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <PlaceInput 
+          onPlaceAdded={this.placeAddedHandler} 
+        />
+        <PlaceList 
+          places={places} 
+          onPlaceSelected={this.placeSelectedHandler} 
+        />
+        <PlaceDetail 
+          selectedPlace={selectedPlace}
+          onPlaceDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
       </View>
     );
   }
@@ -16,8 +69,9 @@ export default class App extends React.Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    padding: 20,
+    backgroundColor: '#fff',
   },
 });
